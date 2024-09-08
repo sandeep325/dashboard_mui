0dc411d994343106559b6e482da246c45eb21e68
sandeep325 <negisandeep9819@gmail.com>
2024-09-08 17:52:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Dashboard from "./Components/Pages/Dashboard/Dashboard";
+import Settings from "./Components/Pages/Settings/Settings";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" exact element={<Dashboard/>}></Route>
+          <Route path="/settings" exact element={<Settings/>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/Components/Pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Sidebar from '../../Sidebar/Sidebar'
+import Box from '@mui/material/Box';
+
+const Dashboard = () => {
+  return (
+    <Box>
+   <Sidebar/>
+      <h3>Dashboard</h3>
+    </Box>
+ 
+  )
+}
+
+export default Dashboard
