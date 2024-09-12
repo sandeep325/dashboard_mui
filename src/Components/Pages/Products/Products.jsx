b9102e51916a9a86d7fb516c245c9772b4cf2922
sandeep325 <negisandeep9819@gmail.com>
2024-09-12 12:19:05 +0530

    create layout for product list

--- src/Components/Pages/Products/Products.jsx
+++ src/Components/Pages/Products/Products.jsx
@@ -2,17 +2,18 @@ import React from 'react'
 import Navbar from '../../Nvbar/Navbar';
 import Box from '@mui/material/Box';
 import Sidebar from '../../Sidebar/Sidebar';
+import ProductsList from './ProductsList';
 
 
 const Products = () => {
     return (
-        <div className='bgcolor' style={{height:"100vh"}}>
+        <div className='bgcolor' >
             <Navbar />
             <Box height={50} />
             <Box sx={{ display: "flex" }}>
             <Sidebar/>
             <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-                <h3>Products</h3>
+                <ProductsList/>
             </Box>
             </Box>
         </div>
