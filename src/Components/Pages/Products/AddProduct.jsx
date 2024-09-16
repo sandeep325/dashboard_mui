32e3c66e7f0045b658f6044b62096fc9e8625887
sandeep325 <negisandeep9819@gmail.com>
2024-09-16 16:38:24 +0530

    create drop down for category

--- src/Components/Pages/Products/AddProduct.jsx
+++ src/Components/Pages/Products/AddProduct.jsx
@@ -5,33 +5,58 @@ import { Button, IconButton } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
 import Grid from '@mui/material/Grid';
 import TextField from '@mui/material/TextField';
+import MenuItem from '@mui/material/MenuItem';
+
+const currencies = [
+    {
+      value: 'Phone',
+      label: 'Phone',
+    },
+    {
+      value: 'Laptop',
+      label: 'Laptop',
+    },
+    {
+      value: 'Headset',
+      label: 'Headset',
+    },
+    {
+      value: 'Keyboard',
+      label: 'Keyboard',
+    },
+    {
+        value: 'Mouse',
+        label: 'Mouse',
+      },
+  ];
+
 const AddProduct = (props) => {
 
     const [name, setName] = useState("");
     const [Price, setPrice] = useState("");
     const [category, setCategory] = useState("");
 
-    const handleChange = (e) => { 
-      if(e.target?.name=="name") {
-        setName(e.target?.value);
-      }
-      else if(e.target?.name=="price") {
-        setPrice(e.target?.value);
-      } 
-      else if(e.target?.name=="category") {
-        setCategory(e.target?.value);
-      }
+    const handleChange = (e) => {
+        if (e.target?.name == "name") {
+            setName(e.target?.value);
+        }
+        else if (e.target?.name == "price") {
+            setPrice(e.target?.value);
+        }
+        else if (e.target?.name == "category") {
+            setCategory(e.target?.value);
+        }
     }
 
 
     const CreateProdcut = (e) => {
-         e.preventDefault();
-         let obj ={
-            'product_name':name,
-            'Price':Price,
-            'category':category
-         }
-         console.log(obj);
+        e.preventDefault();
+        let obj = {
+            'product_name': name,
+            'Price': Price,
+            'category': category
+        }
+        console.log(obj);
     }
     return (<>
         <Box sx={{ m: 2 }} />
@@ -48,15 +73,45 @@ const AddProduct = (props) => {
         <Grid container spacing={2}>
 
             <Grid item xs={12}>
-                <TextField id="outlined-basic" label="Name" name='name' variant="outlined" size='small' sx={{ minWidth: "100%" }}  onChange={handleChange} />
+                <TextField
+                    id="outlined-basic"
+                    label="Name"
+                    name='name'
+                    variant="outlined"
+                    size='small'
+                    sx={{ minWidth: "100%" }}
+                    onChange={handleChange} />
             </Grid>
 
             <Grid item xs={6}>
-                <TextField id="outlined-basic" label="Price"  name='price' type='number' variant="outlined" size='small' sx={{ minWidth: "100%" }} value={Price} onChange={handleChange} />
+                <TextField
+                    id="outlined-basic"
+                    label="Price"
+                    name='price'
+                    type='number'
+                    variant="outlined"
+                    size='small'
+                    sx={{ minWidth: "100%" }}
+                    value={Price} onChange={handleChange} />
             </Grid>
 
             <Grid item xs={6}>
-                <TextField id="outlined-basic" label="Category" name='category' variant="outlined" size='small' sx={{ minWidth: "100%" }} value={category} onChange={handleChange} />
+                <TextField
+                    id="outlined-basic"
+                    label="Category"
+                    name='category'
+                    select
+                    variant="outlined"
+                    size='small'
+                    sx={{ minWidth: "100%" }}
+                    value={category}
+                    onChange={handleChange} >
+                     {currencies.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+                    </TextField>
             </Grid>
 
             <Grid item xs={12}>
